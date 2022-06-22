62c489c1fae1310795a2fff2f1fe7d239a52724b
adjustin <43147830+adjustin@users.noreply.github.com>
2022-06-21 21:53:32 -0700

    Time: 9 ms, Memory: 68.7 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set =new HashSet<>();
+        for (int num : nums){
+            if (set.contains(num)) 
+                return true;
+            else set.add(num);
+            
+        }
+        return false;
+    }
+}
