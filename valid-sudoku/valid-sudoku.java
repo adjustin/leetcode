baa42786d9f6d5b37a5040bf6f37b45e9b6d2160
adjustin <43147830+adjustin@users.noreply.github.com>
2022-06-24 19:47:12 -0700

    Time: 25 ms, Memory: 48.6 MB - LeetHub

--- valid-sudoku/valid-sudoku.java
+++ valid-sudoku/valid-sudoku.java
@@ -3,11 +3,11 @@ class Solution {
         Set seen=new HashSet();
         for (int i =0; i< 9; i++){
             for(int j =0; j<9; j++){
-                char number=board[i][j];
-                if(number!='.'){
-                    if(!seen.add(number+"in row"+ i)||
-                       !seen.add(number+"in column"+ j)||
-                       !seen.add(number+"in block"+ i/3+"-"+j/3))
+               
+                if(board[i][j]!='.'){
+                    if(!seen.add(board[i][j]+"in row"+ i)||
+                       !seen.add(board[i][j]+"in column"+ j)||
+                       !seen.add(board[i][j]+"in block"+ i/3+"-"+j/3))
                        return false;
                         
                 }
