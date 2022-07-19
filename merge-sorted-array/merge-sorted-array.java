157ab7888e5086bf579cdf543a6a2f974eadda57
adjustin <43147830+adjustin@users.noreply.github.com>
2022-07-19 15:30:09 -0700

    Time: 1 ms, Memory: 43.2 MB - LeetHub

--- merge-sorted-array/merge-sorted-array.java
+++ merge-sorted-array/merge-sorted-array.java
@@ -1,15 +1,19 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int end=m+n-1;
-        while(m-1>=0 ||n-1>=0){
-            int x= m-1>=0? nums1[m-1]:Integer.MIN_VALUE; 
-            int y= n-1>=0? nums2[n-1]:Integer.MIN_VALUE;
+        
+        while(m-1>=0||n-1>=0){
+            int x=m-1>=0? nums1[m-1]:Integer.MIN_VALUE;
+            int y=n-1>=0? nums2[n-1]:Integer.MIN_VALUE;
             
-            System.out.println(y);
-            nums1[end]= x>y? x:y;
-            if(x>y) m--;
-            else n--;
-            end--;
+            if(x>y){
+                nums1[end--]=x;
+                m--;
+            }
+            else{
+                nums1[end--]=y;
+                n--;
+            }
         }
         
     }
