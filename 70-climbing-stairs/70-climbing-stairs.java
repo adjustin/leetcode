9424fea09288d53e851aad0aeab1fc4a7685d414
adjustin <43147830+adjustin@users.noreply.github.com>
2022-07-24 17:26:47 -0700

    Time: 0 ms (100.00%), Space: 41 MB (41.10%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -4,13 +4,10 @@ class Solution {
         // if (n==1) return 1;
         // if (n==2) return 2;
         
-        int [] dp= new int [n+2];
+        int[] dp=new int[n+2];
         dp[1]=1;
         dp[2]=2;
         
-        for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
+        for(int i=3;i<=n;i++) dp[i]=dp[i-1]+dp[i-2];
         return dp[n];
-    }
-}
+}}
