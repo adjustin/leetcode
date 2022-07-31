55e80d4da9f6121f81044d5f794000805a36e4fd
adjustin <43147830+adjustin@users.noreply.github.com>
2022-07-31 14:07:32 -0700

    Time: 96 ms, Memory: 66.2 MB - LeetHub

--- shuffle-an-array/shuffle-an-array.java
+++ shuffle-an-array/shuffle-an-array.java
@@ -6,8 +6,8 @@ class Solution {
     
     public Solution(int[] nums) {
         this.nums=nums;
-        this.nums1=nums.clone();
-        rand=new Random();
+        this.nums1=nums.clone(); 
+        rand=new Random(); //           一个新的随机数生成器
     }
     
     public int[] reset() {
@@ -17,7 +17,7 @@ class Solution {
     public int[] shuffle() {
         for(int i=0;i<nums1.length;i++){
             int temp=nums1[i];
-            int j=i+rand.nextInt(nums1.length-i);
+            int j=i+rand.nextInt(nums1.length-i);   //随机生成数字
             nums1[i]=nums1[j];
             nums1[j]=temp;
         }
