0bd183e395515cd7df589d86eed07e12b2b5a430
adjustin <43147830+adjustin@users.noreply.github.com>
2022-08-01 18:36:44 -0700

    Time: 70 ms, Memory: 44.2 MB - LeetHub

--- fizz-buzz/fizz-buzz.js
+++ fizz-buzz/fizz-buzz.js
@@ -8,7 +8,8 @@ var fizzBuzz = function(n) {
         if     (i % 15 == 0) result.push("FizzBuzz");
         else if(i %  3 == 0) result.push("Fizz");
         else if(i %  5 == 0) result.push("Buzz");
-        else result.push(i.toString())
-    }
+        else result.push(String(i))
+    }          
     return result;
 };
+
